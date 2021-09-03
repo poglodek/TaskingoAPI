76437cb548fff7c2a1dfbc511aebec9b4149c920
poglodek <pawelogll@gmail.com>
2021-09-03 16:57:25 +0200

    Adding user contoller

---
+++ TaskingoAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskingoAPI.Controllers
+{
+    [ApiController]
+    [Route("/User")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("GetAll")]
+        public ActionResult<string> GetClientsList()
+        {
+            var users = "All Users";
+            return Ok(users);
+        }
+
+    }
+}
